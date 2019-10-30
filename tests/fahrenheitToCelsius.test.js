76985ddb18b9ac3080419552eebd6f492895a53d
zabahey <zabahey.sukoi@gmail.com>
2019-10-30 22:16:23 +0700

    Created test suite

---
+++ tests/fahrenheitToCelsius.test.js
@@ -0,0 +1,10 @@
+describe.only('the fahrenheit to celsius canary spec', () => {
+	it('shows the infrastructure works', () => {
+		expect(true).toBe(true)
+	})
+	describe('fahrenheit to celsius converter behaves as follows', () => {
+		// it('212 degrees fahrenheit is 100 degrees celsius')
+		// it('32 degrees fahrenheit is 0 degrees celsius')
+		// it('50 degrees fahrenheit is 10 degrees celsius')
+	})
+})
